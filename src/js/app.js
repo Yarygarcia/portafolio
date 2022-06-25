22ce9ca293b191f59561b263ef92d9d480c605ac
yarygarcia <yarygarciaq@hotmail.com>
2022-06-24 19:31:01 -0500

    Nueva imagen al carrusel

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ const punto = d.querySelectorAll('.punto')
 punto.forEach((cadaPunto, i)=>{
     punto[i].addEventListener('click', ()=>{
         let posicion = i
-        let operacion = posicion * -50
+        let operacion = posicion * -33.4
         grande.style.transform = `translateX(${ operacion }%)`
         punto.forEach((cadaPunto, i) =>{
             punto[i].classList.remove('activo')
